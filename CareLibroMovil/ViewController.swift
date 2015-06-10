6a9ea1576c4a9838d441c38ec8d447dbf0b2a932
santiago carmona gonzalez <Santicarmo@santiagos-MacBook-Air-3.local>
2015-06-10 18:51:17 -0500

    Initial Commit

---
+++ CareLibroMovil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareLibroMovil
+//
+//  Created by santiago carmona gonzalez on 6/10/15.
+//  Copyright (c) 2015 santiago carmona gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
